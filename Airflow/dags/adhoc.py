50fb001014ce0f6b74a45d9f6a7444ca1408a7a1
ajinabraham123 <lnu.aj@northeastern.edu>
2023-04-25 19:19:01 -0400

    Updated the airflow code. Still there is an issue with the JWT Tokken in airflow

--- Airflow/dags/adhoc.py
+++ Airflow/dags/adhoc.py
@@ -1,85 +1,97 @@
-from datetime import timedelta
+import requests
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from datetime import datetime
-import requests
 
-from airflow.models import BaseOperator
-from airflow.utils.decorators import apply_defaults
 
+BASE_URL = "http://host.docker.internal:8000"
+ # Replace with your FastAPI app URL
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2023, 4, 25),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=5),
 }
 
-
 dag = DAG(
-    'fastapi_endpoints',
+    "fastapi_dag",
     default_args=default_args,
-    description='DAG for FastAPI endpoints',
+    description="Trigger FastAPI endpoints with Airflow",
     schedule_interval=timedelta(days=1),
-    start_date=datetime(2023, 4, 24),
     catchup=False,
 )
 
 
-class MyCustomOperator(BaseOperator):
+def call_fastapi_endpoint(endpoint, token):
+    url = f"{BASE_URL}/{endpoint}"
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(url, headers=headers)
 
-    @apply_defaults
-    def __init__(self, endpoint, *args, **kwargs):
-        super(MyCustomOperator, self).__init__(*args, **kwargs)
-        self.endpoint = endpoint
-
-    def execute(self, context):
-        url = f"http://localhost:8000/{self.endpoint}"
-        response = requests.get(url)
-        # Do something with the response, e.g., parse data and store in a database
+    if response.status_code == 200:
+        print(f"Successfully triggered {endpoint}.")
+    else:
+        print(f"Error triggering {endpoint}: {response.status_code}, {response.text}")
 
 
+def get_auth_token(username, password):
+    url = f"{BASE_URL}/token"
+    data = {"username": username, "password": password}
+    response = requests.post(url, data=data)
 
-def stock_data_pull_task(**kwargs):
-    print("stock_data_pull_task executed")
-    url = "http://localhost:8000/stock-data-scrape"
-    response = requests.get(url)
-    stock_data = response.json()
-    print(stock_data)
-
-
-def stock_newsletter_task(**kwargs):
-    url = "http://localhost:8000/stock-newsletter"
-    response = requests.get(url)
-    newsletter = response.text
-    print(newsletter)
+    if response.status_code == 200:
+        token = response.json().get("access_token")
+        return token
+    else:
+        print(f"Error getting auth token: {response.status_code}, {response.text}")
+        return None
 
 
-def stock_recommendation_task(**kwargs):
-    url = "http://localhost:8000/stock-recommendation"
-    response = requests.get(url)
-    recommendation = response.json()
-    print(recommendation)
+def call_get_auth_token():
+    token = get_auth_token("admin", "admin1234")
+    return token
 
 
-stock_data_pull_operator = MyCustomOperator(
-    task_id='stock_data_pull',
-    endpoint='stock-data-scrape',
+def call_stock_data_scrape(token):
+    call_fastapi_endpoint("stock-data-scrape", token)
+
+
+def call_stock_recommendation(token):
+    call_fastapi_endpoint("stock-recommendation", token)
+
+
+def call_stock_newsletter(token):
+    call_fastapi_endpoint("stock-newsletter", token)
+
+
+get_auth_token_operator = PythonOperator(
+    task_id="get_auth_token_task",
+    python_callable=call_get_auth_token,
     dag=dag,
 )
 
-stock_newsletter_operator = MyCustomOperator(
-    task_id='stock_newsletter',
-    endpoint='stock-newsletter',
+stock_data_scrape_operator = PythonOperator(
+    task_id="stock_data_scrape_task",
+    python_callable=call_stock_data_scrape,
+    op_args=["{{ task_instance.xcom_pull(task_ids='get_auth_token_task') }}"],
     dag=dag,
 )
 
-stock_recommendation_operator = MyCustomOperator(
-    task_id='stock_recommendation',
-    endpoint='stock-recommendation',
+stock_recommendation_operator = PythonOperator(
+    task_id="stock_recommendation_task",
+    python_callable=call_stock_recommendation,
+    op_args=["{{ task_instance.xcom_pull(task_ids='get_auth_token_task') }}"],
     dag=dag,
 )
 
-stock_recommendation_operator >>  stock_newsletter_operator
+stock_newsletter_operator = PythonOperator(
+    task_id="stock_newsletter_task",
+    python_callable=call_stock_newsletter,
+    op_args=["{{ task_instance.xcom_pull(task_ids='get_auth_token_task') }}"],
+    dag=dag,
+)
+
+get_auth_token_operator >> stock_data_scrape_operator >> stock_recommendation_operator >> stock_newsletter_operator
