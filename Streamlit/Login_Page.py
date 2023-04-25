77c94de134c157249b4674feb63442dd09b70662
Meet Doshi <doshi.mee@northeastern.edu>
2023-04-25 18:34:08 -0400

    added plan and upgrading plan

--- Streamlit/Login_Page.py
+++ Streamlit/Login_Page.py
@@ -52,9 +52,9 @@ with st.sidebar:
     response = requests.get(f"{BASE_URL}/user/details?username={st.session_state.username}", headers={'Authorization' : f"Bearer {st.session_state['access_token']}"})
     if response.status_code == 200:
         user_plan = json.loads(response.text)
-        st.write("Your plan: ", user_plan[0])
+        st.write("Your plan: ", user_plan)
     else:
-        st.write('')
+        st.write('Your plan: None')
 
     logout_button = st.button('Log Out')
     if logout_button:
@@ -91,43 +91,16 @@ if st.session_state.logged_in == False:
                     st.session_state.access_token = json_data['access_token']
                     st.session_state.username = username
                     st.success("Login successful")
-                    # st.session_state.page = 'Stock_Dashboard'
-
+                    
                 elif response.status_code == 401:
                     st.warning('Please register yourself first.')
-                    # switch_page('Register_Page')
-                    # st.session_state.page = 'Register_Page'
-
+                    
                 else:
                     st.error("""Incorrect username or password entered !! \n Please check again your user credentails !!""")
 
     if forgot_password:
         switch_page('Forgot_Password')
-        # st.session_state.page = 'Forgot_Password'
 
 if st.session_state.logged_in == True:
     with st.spinner("Loading..."):
         switch_page('Stock_Dashboard')
-
-# def main():
-#     if 'page' not in st.session_state:
-#         st.session_state.page = 'Login'
-    
-#     if 'logged_in' not in st.session_state:
-#         st.session_state.logged_in = False
-#         st.session_state.access_token = ''
-#         st.session_state.full_name = ''
-#         st.session_state.email = ''
-#         st.session_state.username = ''
-#         st.session_state.password = ''
-
-#     pages = {
-#         "Login": login_page,
-#         # Add other page functions here, e.g. "Register": register_page, "Forgot_Password": forgot_password_page
-#     }
-
-#     pages[st.session_state.page]()
-
-
-# if __name__ == "__main__":
-#     main()
