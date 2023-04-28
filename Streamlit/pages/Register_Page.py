3fdecb6fad2374b1890896c19d3beaed7e066814
ajinabraham123 <lnu.aj@northeastern.edu>
2023-04-28 00:14:51 -0400

    updated changes

--- Streamlit/pages/Register_Page.py
+++ Streamlit/pages/Register_Page.py
@@ -8,12 +8,7 @@ from dotenv import load_dotenv
 from streamlit_extras.switch_page_button import switch_page
 
 load_dotenv()
-app_status = os.environ.get('APP_STATUS')
-
-if app_status == "DEV":
-    BASE_URL = "http://localhost:8000"
-elif app_status == "PROD":
-    BASE_URL = "http://:8000"
+BASE_URL = os.getenv("API_URL", "http://localhost:8000")
 
 clientLogs = boto3.client('logs',
                         region_name='us-east-1',
